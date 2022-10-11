70652b8612ca1c007666042d1b3e1cbc51013cdc
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-11 09:44:20 +0300

    adding subscrition route

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,18 @@
 class SubscriptionsController < ApplicationController
+
+    def create
+        subscription = Subscription.create!(subscription_params)
+        if subscription.valid?
+            render json: subscription, status: :created
+        else 
+            render json: { errors: subscription.errors }, status: :unprocessable_entity
+        end 
+    end
+
+    private 
+
+    def subscription_params
+        params.permit(:name , :provider, :category, :amount , :start_date, :billing_cycle) 
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :subscriptions only
+  resources :subscriptions, only: [:create]
   resources :reminders
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
