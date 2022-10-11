c3e05e41cbe59dcd1645fbb3ab2f97ab10ebcc60
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-11 09:54:20 +0300

    milestone: created create route for reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,2 +1,18 @@
 class RemindersController < ApplicationController
+
+    def create
+        reminder = Reminder.create!(reminder_params)
+        if reminder.valid?
+            render json: reminder, :status: :created 
+        else
+            render json: { errors: reminder.errors }, status: :unprocessable_entity 
+        end 
+    end
+
+
+    private
+
+    def reminder_params
+        params.permit(:name , :status ,:due_date, :user_id)
+    end 
 end
